6bdb50be56725180fa85aa0340206dbe0b531151
hephaes <39906054+amhcam@users.noreply.github.com>
2023-02-28 20:50:10 +0800

    Update Test_validate_username.c

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,8 +18,8 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    char username_var[];
-    char comparend[];
+    char username_var;
+    char comparend;
     username_var = my_username();
     comparend = malloc_username_from_conf_file();
     TEST_ASSERT_EQUAL_STRING_MESSAGE(username_var, comparend, "Message from Unity Test");
